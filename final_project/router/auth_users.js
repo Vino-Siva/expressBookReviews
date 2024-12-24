2b2e631bcab7e1198539099472d57a40d99e1488
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2024-12-24 11:52:11 +0530

    Refactor authentication functions and improve code formatting in auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,28 +1,39 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username) => {
+  //write code to check if username is valid
+  let user = users.find((user) => user.username === username);
+  if (user) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) => {
+  //write code to check if username and password match the one we have in records
+  let user = users.find((user) => user.username === username);
+  if (user && user.password === password) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-//only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  // only registered users can login
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.authenticated = regd_users;
